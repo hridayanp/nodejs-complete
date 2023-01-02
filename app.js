314299571d4dedad67f03074a6dcf6fb7ec5f086
hridayanp <hridayanphukan123@gmail.com>
2023-01-02 21:13:25 +0530

    added statically served css routes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -9,11 +9,10 @@ const errorRoutes = require('./routes/404')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use(errorRoutes)
 
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000)
